6d08861c4df63476cdf90a834d331e4aaf7ef42e
cleomjr <cleomjr@gmail.com>
2019-01-08 01:34:56 -0200

    Swagger configured for the API documentation

---
+++ src/main/java/com/santandertecnologia/testbackjava/api/SwaggerConfiguration.java
@@ -0,0 +1,24 @@
+package com.santandertecnologia.testbackjava.api;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        super.addResourceHandlers(registry);
+    }
+}
