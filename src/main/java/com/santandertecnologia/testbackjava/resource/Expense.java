4015df1ca88654156087cb9e27702e3e66b96199
cleomjr <cleomjr@gmail.com>
2019-01-08 01:32:36 -0200

    Added Expense resource for "gastos" representation

--- src/main/java/com/santandertecnologia/testbackjava/resource/Expense.java
+++ src/main/java/com/santandertecnologia/testbackjava/resource/Expense.java
@@ -1,10 +1,11 @@
 package com.santandertecnologia.testbackjava.resource;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 import java.io.Serializable;
-import java.time.OffsetDateTime;
+import java.time.LocalDateTime;
 
 @RedisHash("Expense")
 public class Expense implements Serializable {
@@ -15,49 +16,60 @@ public class Expense implements Serializable {
     private String description;
     private Double value;
     private int userCode;
-    private OffsetDateTime date;
+    private LocalDateTime date;
     private String category;
 
+    @JsonProperty("id")
     public int getId() {
         return id;
     }
 
+    @JsonProperty("categoria")
     public String getCategory() {
         return category;
     }
 
+    @JsonProperty("categoria")
     public void setCategory(String category) {
         this.category = category;
     }
 
+    @JsonProperty("valor")
     public Double getValue() {
         return value;
     }
 
+    @JsonProperty("codigousuario")
     public int getUserCode() {
         return userCode;
     }
 
-    public OffsetDateTime getDate() {
+    @JsonProperty("data")
+    public LocalDateTime getDate() {
         return date;
     }
 
+    @JsonProperty("descricao")
     public String getDescription() {
         return description;
     }
 
-    public void setDate(OffsetDateTime date) {
+    @JsonProperty("data")
+    public void setDate(LocalDateTime date) {
         this.date = date;
     }
 
+    @JsonProperty("descricao")
     public void setDescription(String description) {
         this.description = description;
     }
 
+    @JsonProperty("codigousuario")
     public void setUserCode(int userCode) {
         this.userCode = userCode;
     }
 
+    @JsonProperty("valor")
     public void setValue(Double value) {
         this.value = value;
     }
--- src/main/java/com/santandertecnologia/testbackjava/resource/ExpenseRepository.java
+++ src/main/java/com/santandertecnologia/testbackjava/resource/ExpenseRepository.java
@@ -3,12 +3,13 @@ package com.santandertecnologia.testbackjava.resource;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.time.LocalDateTime;
 
 @Repository
 public interface ExpenseRepository extends CrudRepository<Expense, String> {
 
-    List<Expense> findByCategoryIgnoreCase(String category);
-    List<Expense> findByDescriptionIgnoreCase(String description);
+    Iterable<Expense> findByCategoryIgnoreCase(String category);
+    Iterable<Expense> findByDescriptionIgnoreCase(String description);
+    Iterable<Expense> findByDate(LocalDateTime date);
 
 }
